fdfb761f4ca5f0799f12337abf574aba50cb62c5
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-05-19 01:02:32 +0300

    init

--- src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import css from "./Title.module.css"
-
-interface TitpleProps {
-    text: string;
-}
-
-export default function Title({ text }: TitpleProps) {
-    return <h2 className={css.title}>{text}</h2>
-}
